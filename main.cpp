1b62a51d4499edc98df27381aa8332390073eee6
Keshyyyy <keshmuthu1543@gmail.com>
2025-04-03 14:03:57 -0700

    update commit

--- main.cpp
+++ main.cpp
@@ -1,25 +1,87 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+bool power_of_two(int number) {
+    if (number == 0)
+        return false;
+    while(number > 1){
+        if(number % 2 == 0)
+            number = number / 2;
+        else
+            return false;
     }
-
-    return 0;
+    return true;
 }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+bool power_of_two_noloop(int number) {
+    if ((number > 0) && ((number & (number - 1)) == 0))
+        return true;
+    return false;
+}
+
+void GCD(int num, int num2) {
+    int temp = 0;
+    int numerator = num;
+    int denominator = num2;
+
+    printf("%d/%d reduced is \n", numerator, denominator);
+
+    while (num2 != 0) {
+        temp = num2;
+        num2 = num % num2;
+        num = temp;
+    }
+
+    int gcd = num;
+
+    numerator = numerator / gcd;
+    denominator = denominator / gcd;
+
+    std::cout << numerator << "/" << denominator << std::endl;
+
+
+}
+
+bool is_prime(int number) {
+    if(number <= 1)
+        return false;
+    if (number % 2 == 0)
+        return false;
+    for (int i = 2; i < number; i++) {
+        if (number % i == 0)
+            return false;
+    }
+
+    return true;
+
+}
+
+void test_power_of_two(int num) {
+    std::cout << "Testing power of two function using loops" << std::endl;
+    if (power_of_two(num))
+        printf("%d is a power of two - PASS\n", num);
+    else
+        printf("%d is not a power of two - FAIL\n", num);
+
+    std::cout << "\nTesting power of two function using bitwise logic" << std::endl;
+    if (power_of_two_noloop(num))
+        printf("%d is a power of two - PASS\n", num);
+    else
+        printf("%d is not a power of two - FAIL\n", num);
+
+}
+
+void test_prime_numbers(int num) {
+    std::cout << "\nTesting prime number function" << std::endl;
+    if (is_prime(num))
+        printf("%d is a prime number - PASS\n", num);
+    else
+        printf("%d is not a prime number - FAIL\n", num);
+}
+
+int main() {
+    test_power_of_two(16);
+    test_prime_numbers(2);
+
+    return 0;
+
+}
